3ac1f2c1b4f987b1700718eda982d92327099edd
Alan Avila <alan.avila04@inacapmail.cl>
2025-10-23 15:30:52 -0300

    update models

--- inventario_escolar/equipos/models.py
+++ inventario_escolar/equipos/models.py
@@ -20,15 +20,11 @@ class Equipo(models.Model):
 
     #Atributos definidos con model
     nombre = models.CharField(max_length=100)
-    categoria = models.CharField(choices=CATEGORIAS, default='')
-    estado = models.CharField(choices=ESTADO, default='operativo')
+    categoria = models.CharField(max_length=50, choices=CATEGORIAS, default='')
+    estado = models.CharField(max_length=50, choices=ESTADO, default='operativo')
     fecha_ingreso = models.DateField(auto_now_add=True)
     ubicacion = models.CharField(max_length=100)
 
     #Función str para obtener
     def __str__(self):
-        return (f'Nombre: {self.nombre}\n'
-        f'Categoria: {self.categoria}\n'
-               f'Estado: {self.estado}\n'
-               f'Fecha Ingreso: {self.fecha_ingreso}\n'
-               f'Ubicación: {self.ubicacion}')
+        return f"{self.nombre} ({self.categoria}) - {self.estado}"
