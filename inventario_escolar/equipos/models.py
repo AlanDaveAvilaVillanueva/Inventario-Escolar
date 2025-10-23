3483ffa3b78d22bdc5968dcd4cceaa0145de493f
Alan Avila <alan.avila04@inacapmail.cl>
2025-10-23 15:29:17 -0300

    Update models.py

--- inventario_escolar/equipos/models.py
+++ inventario_escolar/equipos/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
+# Tabla models de Equipo
 class Equipo(models.Model):
 
+    #Variables para almacenar las tuplas que voy a designar mas adelante en choices
     CATEGORIAS = [
         ('proyector', 'Proyector'),
         ('notebook', 'Notebook'),
@@ -14,11 +17,18 @@ class Equipo(models.Model):
         ('dado_de_baja', 'Dado de Baja')
 
     ]
+
+    #Atributos definidos con model
     nombre = models.CharField(max_length=100)
     categoria = models.CharField(choices=CATEGORIAS, default='')
     estado = models.CharField(choices=ESTADO, default='operativo')
     fecha_ingreso = models.DateField(auto_now_add=True)
     ubicacion = models.CharField(max_length=100)
 
+    #Función str para obtener
     def __str__(self):
-        return self.nombre
+        return (f'Nombre: {self.nombre}\n'
+        f'Categoria: {self.categoria}\n'
+               f'Estado: {self.estado}\n'
+               f'Fecha Ingreso: {self.fecha_ingreso}\n'
+               f'Ubicación: {self.ubicacion}')
