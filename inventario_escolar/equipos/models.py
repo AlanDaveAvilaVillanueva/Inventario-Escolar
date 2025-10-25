c107e9489c9adeaa3e7722a2188c5275355e23c0
Alan Avila <alan.avila04@inacapmail.cl>
2025-10-25 02:23:09 -0300

    Update models.py

--- inventario_escolar/equipos/models.py
+++ inventario_escolar/equipos/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Equipo(models.Model):
 
     #Variables para almacenar las tuplas que voy a designar mas adelante en choices
+    CATEGORIAS = [
+        ('proyector','Proyector'),
+        ('notebook','Notebook'),
+        ('impresora','Impresora')
+    ]
+    
     ESTADO = [
         ('operativo', 'Operativo'),
         ('en_reparacion', 'En Reparación'),
@@ -14,7 +20,7 @@ class Equipo(models.Model):
 
     #Atributos definidos con model
     nombre = models.CharField(max_length=100)
-    categoria = models.ForeignKey(Categorias, on_delete=models.PROTECT, related_name='categorias')
+    categoria = models.CharField(max_length=50, choices=CATEGORIAS, default='')
     estado = models.CharField(max_length=50, choices=ESTADO, default='operativo')
     fecha_ingreso = models.DateField(auto_now_add=True)
     ubicacion = models.CharField(max_length=100)
@@ -23,8 +29,3 @@ class Equipo(models.Model):
     def __str__(self):
         return f"{self.nombre} ({self.categoria}) - {self.estado}"
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.nombre}
